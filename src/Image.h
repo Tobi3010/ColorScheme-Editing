fb49bb90456a9321a0d76f6df17ea2700e909467
Tobi3010 <tobiasandersen62@gmail.com>
2025-02-08 17:53:02 +0100

    image initialization

---
+++ src/Image.h
@@ -0,0 +1,25 @@
+#include <stdint.h>
+#include <cstdio>
+
+enum ImageType {
+    PNG, JPG, BMP, TGA
+};
+
+struct Image {
+    uint8_t* data = NULL;
+    size_t size = 0;
+    int w;
+    int h;
+    int channels;                       // How many color values per pixel
+
+    Image(const char* filename);
+    Image(int w, int h, int channels);
+    Image(const Image& img);
+    ~Image();
+
+    bool write(const char* filename);
+    bool read(const char* filename);
+
+    ImageType getFileType(const char* filename);
+   
+};
